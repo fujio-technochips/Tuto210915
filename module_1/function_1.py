34b21460c0ccdf83106df025cc3fde17103419de
fujio takemura <t_fujio@technochips.co.jp>
2021-09-09 16:31:12 +0900

    さらにさらに

--- module_1/function_1.py
+++ module_1/function_1.py
@@ -6,7 +6,7 @@ import tkinter.messagebox
 def get_filename():
     root=tkinter.Tk()    # 一番外に必要
     root.withdraw()    # 一番外に必要
-    root.attributes("-topmost", True)       #<-最前面
+    root.attributes("-topmost", True)       #<-最前面さらにマスターに追加
     fTyp=[('pynote','*.ipynb'),('python','*.py'),('全て','*.*')]
     f =  tkinter.filedialog.askopenfilename(filetypes=fTyp)
     #print(f)
