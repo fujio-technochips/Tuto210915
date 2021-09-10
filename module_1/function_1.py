50a7c80acb5a3232e3fa329dfd240b0ddafaa7ca
fujio takemura <t_fujio@technochips.co.jp>
2021-09-10 15:32:53 +0900

    プルリクエスト評価

--- module_1/function_1.py
+++ module_1/function_1.py
@@ -6,12 +6,7 @@ import tkinter.messagebox
 def get_filename():
     root=tkinter.Tk()    # 一番外に必要
     root.withdraw()    # 一番外に必要
-<<<<<<< HEAD
     root.attributes("-topmost", True)       #<-最前面さらにマスターに追加さらにマスターに追加さらにマスターに追加
-=======
-    root.attributes("-topmost", True)       #<-最前面
-    #競合するように変更した
->>>>>>> a26b11ca1d15183276bcbdeac081d19f33a6e5d9
     fTyp=[('pynote','*.ipynb'),('python','*.py'),('全て','*.*')]
     f =  tkinter.filedialog.askopenfilename(filetypes=fTyp)
     #print(f)
--- module_2/function_2.py
+++ module_2/function_2.py
@@ -1,5 +1,5 @@
 #上記の%%writefile sub1.pyｈ
-print("function_2.pyをロードしましたよ～xx")
+print("function_2.pyをロードしましたよ～xx-----")
 import os
 def ExchangeExtension(fname,newext):
 	# よくわからんけどコメント追加
